94bb12b8b2d5ea7cb2a6058f524093bccf017878
slomusti <musti@wlan-si.net>
2017-11-02 20:47:25 +0100

    dev

--- gpio_example.py
+++ gpio_example.py
@@ -6,8 +6,21 @@ import time
 
 #logging.basicConfig(format='%(levelname)s-%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG,filename='/App/gpio.log')
 
+timer_en_pin = 17
+rtc_en_pin = 22
+
 # Set GPIO mode: GPIO.BCM or GPIO.BOARD
 GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(timer_en_pin, GPIO.IN)
+GPIO.setup(rtc_en_pin, GPIO.IN)
+
+# Check for power-up scenario
+timer_en_state = GPIO.input(timer_en_pin)
+rtc_en_state = GPIO.input(rtc_en_pin)
+#convert variable into str
+print("Timer EN state " + str(timer_en_state))
+print("RTC EN state " + str(rtc_en_state))
 
 # GPIO pins list based on GPIO.BOARD
 # gpioList1 = [3,5,7,8,10,11,12,13,15]
@@ -19,6 +32,6 @@ GPIO.setmode(GPIO.BCM)
 
 while True:
 	# Change gpio pins in list 1 from low to high and list 2 from high to low
-
+    time.sleep(1)
 # Reset all gpio pin
 GPIO.cleanup()

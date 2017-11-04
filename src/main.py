acfd3b25db72e04cda40f5a9bf53040d17a49cec
slomusti <musti@wlan-si.net>
2017-11-04 18:08:55 +0100

    dev

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import time
 
 import BQ2429x
@@ -7,68 +10,94 @@ import time
 
 
 def main():
-	#debug_main()
-	debug_it_all()
-	time.sleep(6)
+
+    # debug_main()
+
+    debug_it_all()
+    time.sleep(6)
+
 
 def debug_main():
 
-	print "==============================================="
-	print "BQ2429x  : status - VBUS_STAT : " + str(sensor_bq.get_status(BQ2429x.VBUS_STAT))
-	print "BQ2429x  : status - CHRG_STAT : " + str(sensor_bq.get_status(BQ2429x.CHRG_STAT))
-	print "MCP3021  : status - voltage --: " + str(sensor_mcp.get_voltage()) + "V"
+    print '==============================================='
+    print 'BQ2429x  : status - VBUS_STAT : ' \
+        + str(sensor_bq.get_status(BQ2429x.VBUS_STAT))
+    print 'BQ2429x  : status - CHRG_STAT : ' \
+        + str(sensor_bq.get_status(BQ2429x.CHRG_STAT))
+    print 'MCP3021  : status - voltage --: ' \
+        + str(sensor_mcp.get_voltage()) + 'V'
+
 
 def debug_it_all():
 
-	print "==============================================="
-	print "BQ2429x  : status - VSYS ------- : " + str(sensor_bq.get_status(BQ2429x.VSYS_STAT))
-	print "BQ2429x  : status - THERM_STAT - : " + str(sensor_bq.get_status(BQ2429x.THERM_STAT))
-	print "BQ2429x  : status - PG_STAT ---- : " + str(sensor_bq.get_status(BQ2429x.PG_STAT))
-	print "BQ2429x  : status - DPM_STAT --- : " + str(sensor_bq.get_status(BQ2429x.DPM_STAT))
-	print "BQ2429x  : status - CHRG_STAT -- : " + str(sensor_bq.get_status(BQ2429x.CHRG_STAT))
-	print "BQ2429x  : status - VBUS_STAT -- : " + str(sensor_bq.get_status(BQ2429x.VBUS_STAT))
-	print "BQ2429x  : fault - NTC_FAULT --- : " + str(sensor_bq.get_faults(BQ2429x.NTC_FAULT))
-	print "BQ2429x  : fault - BAT_FAULT --- : " + str(sensor_bq.get_faults(BQ2429x.BAT_FAULT))
-	print "BQ2429x  : fault - CHRG_FAULT -- : " + str(sensor_bq.get_faults(BQ2429x.CHRG_FAULT))
-	print "BQ2429x  : fault - BOOST_FAULT - : " + str(sensor_bq.get_faults(BQ2429x.BOOST_FAULT))
-	print "BQ2429x  : fault - WATCHDOG_FAULT: " + str(sensor_bq.get_faults(BQ2429x.WATCHDOG_FAULT))
-	print "MCP3021  : status - voltage -----: " + str(sensor_mcp.get_voltage())
+    print '==============================================='
+    print 'BQ2429x  : status - VSYS ------- : ' \
+        + str(sensor_bq.get_status(BQ2429x.VSYS_STAT))
+    print 'BQ2429x  : status - THERM_STAT - : ' \
+        + str(sensor_bq.get_status(BQ2429x.THERM_STAT))
+    print 'BQ2429x  : status - PG_STAT ---- : ' \
+        + str(sensor_bq.get_status(BQ2429x.PG_STAT))
+    print 'BQ2429x  : status - DPM_STAT --- : ' \
+        + str(sensor_bq.get_status(BQ2429x.DPM_STAT))
+    print 'BQ2429x  : status - CHRG_STAT -- : ' \
+        + str(sensor_bq.get_status(BQ2429x.CHRG_STAT))
+    print 'BQ2429x  : status - VBUS_STAT -- : ' \
+        + str(sensor_bq.get_status(BQ2429x.VBUS_STAT))
+    print 'BQ2429x  : fault - NTC_FAULT --- : ' \
+        + str(sensor_bq.get_faults(BQ2429x.NTC_FAULT))
+    print 'BQ2429x  : fault - BAT_FAULT --- : ' \
+        + str(sensor_bq.get_faults(BQ2429x.BAT_FAULT))
+    print 'BQ2429x  : fault - CHRG_FAULT -- : ' \
+        + str(sensor_bq.get_faults(BQ2429x.CHRG_FAULT))
+    print 'BQ2429x  : fault - BOOST_FAULT - : ' \
+        + str(sensor_bq.get_faults(BQ2429x.BOOST_FAULT))
+    print 'BQ2429x  : fault - WATCHDOG_FAULT: ' \
+        + str(sensor_bq.get_faults(BQ2429x.WATCHDOG_FAULT))
+    print 'MCP3021  : status - voltage -----: ' \
+        + str(sensor_mcp.get_voltage())
 
 
 if __name__ == '__main__':
 
-	# referencing the sensors
-	sensor_bq	= BQ2429x.BQ2429x()
-	sensor_mcp	= MCP3021.MCP3021()
+    # referencing the sensors
 
-	# configure GPIOs
-	timer_en_pin = 17
-	rtc_en_pin = 22
-	self_en_pin = 18
+    sensor_bq = BQ2429x.BQ2429x()
+    sensor_mcp = MCP3021.MCP3021()
 
-	timer_done_pin = 27
+    # configure GPIOs
 
-	# Set GPIO mode: GPIO.BCM or GPIO.BOARD
-	GPIO.setmode(GPIO.BCM)
+    timer_en_pin = 17
+    rtc_en_pin = 22
+    self_en_pin = 18
 
-	GPIO.setup(timer_en_pin, GPIO.IN)
-	GPIO.setup(rtc_en_pin, GPIO.IN)
+    timer_done_pin = 27
 
-	# Check for power-up scenario
-	timer_en_state = GPIO.input(timer_en_pin)
-	rtc_en_state = GPIO.input(rtc_en_pin)
+    # Set GPIO mode: GPIO.BCM or GPIO.BOARD
 
-	# Self-enable
-	GPIO.setup(self_en_pin, GPIO.OUT)
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(timer_en_pin, GPIO.IN)
+    GPIO.setup(rtc_en_pin, GPIO.IN)
+
+    # Check for power-up scenario
+
+    timer_en_state = GPIO.input(timer_en_pin)
+    rtc_en_state = GPIO.input(rtc_en_pin)
+
+    # Self-enable
+
+    GPIO.setup(self_en_pin, GPIO.OUT)
     GPIO.write(self_en_pin, 1)
 
-	# Assert done for timer
-	GPIO.setup(timer_done_pin, GPIO.OUT)
-	GPIO.write(timer_done_pin, HIGH)
+    # Assert done for timer
 
-	#convert variable into str
-	print("Timer EN state " + str(timer_en_state))
-	print("RTC EN state " + str(rtc_en_state))
+    GPIO.setup(timer_done_pin, GPIO.OUT)
+    GPIO.write(timer_done_pin, HIGH)
 
-	while 1:
-		main()
+    # convert variable into str
+
+    print 'Timer EN state ' + str(timer_en_state)
+    print 'RTC EN state ' + str(rtc_en_state)
+
+    while 1:
+        main()

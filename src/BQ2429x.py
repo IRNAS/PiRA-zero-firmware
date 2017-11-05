e1f68da57f967a617387ec5fcc487e9f884ef717
slomusti <musti@wlan-si.net>
2017-11-05 10:44:37 +0100

    dev

--- src/BQ2429x.py
+++ src/BQ2429x.py
@@ -180,11 +180,11 @@ class BQ2429x(object):
 		#					- PRECH_CURRENT_DEFAULT (0001)
 
 		try:
-			#current_value = i2c.read_byte_data(BQ2429x_I2CADDR,BQ2429x_CHARGE_TERM_CTRL_ADDR)						# read the register
+			current_value = i2c.read_byte_data(BQ2429x_I2CADDR,BQ2429x_CHARGE_TERM_CTRL_ADDR)						# read the register
 			#binary_value[3]=timer_en
-			writing_value = int(str(10010010),2)
+			#writing_value = int(str(10010010),2)
 
-			i2c.write_byte_data(BQ2429x_I2CADDR,BQ2429x_CHARGE_TERM_CTRL_ADDR, writing_value)					    # write to register
+			#i2c.write_byte_data(BQ2429x_I2CADDR,BQ2429x_CHARGE_TERM_CTRL_ADDR, writing_value)					    # write to register
 			#current_value = i2c.read_byte_data(BQ2429x_I2CADDR,BQ2429x_CHARGE_TERM_CTRL_ADDR)						# read the register
 
 			#current_value = self.check8bit(current_value)

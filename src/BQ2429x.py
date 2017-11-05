f6210c03bbe29e5aaec9d639373a537108b4215c
slomusti <musti@wlan-si.net>
2017-11-05 14:45:42 +0100

    dev

--- src/BQ2429x.py
+++ src/BQ2429x.py
@@ -160,13 +160,13 @@ class BQ2429x(object):
         try:
             writing_value = int(str(termination) + str(precharge))                                    # combine the value and convert to int
             #i2c.write_byte_data(BQ2429x_I2CADDR,BQ2429x_PRECHARGE_CTRL_ADDR, writing_value)                        # write to register
-            current_value = i2c.read_byte_data(BQ2429x_I2CADDR,BQ2429x_PRECHARGE_CTRL_ADDR)                        # read the register
-            current_value = self.check8bit(current_value)
+            #current_value = i2c.read_byte_data(BQ2429x_I2CADDR,BQ2429x_PRECHARGE_CTRL_ADDR)                        # read the register
+            #current_value = self.check8bit(current_value)
 
-            if int(hex(current_value)[2:]) == writing_value:                                        # comapre them
-                return str(writing_value) + " - Success"                                            # success!
-            else:
-                return str(writing_value) + " - ERROR!"                                                # not the same!
+            #if int(hex(current_value)[2:]) == writing_value:                                        # comapre them
+            #    return str(writing_value) + " - Success"                                            # success!
+            #else:
+            #    return str(writing_value) + " - ERROR!"                                                # not the same!
 
         except Exception as e:
             print (e)

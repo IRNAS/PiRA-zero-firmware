2e9a459944bded246ae3277d9a0e3a3abbe7c19b
slomusti <musti@wlan-si.net>
2017-11-07 15:50:16 +0100

    dev

--- syncTime.sh
+++ syncTime.sh
@@ -5,34 +5,6 @@
 #
 
 
-# get RTC time
-rtctime="$(get_rtc_time)"
-
-# if RTC time is OK, write RTC time to system first
-if [[ $rtctime != *"1999"* ]] && [[ $rtctime != *"2000"* ]]; then
-  rtc_to_system
-fi
-
-#if $(has_internet) ; then
- # # now take new time from NTP
-  #log 'Internet detected, apply NTP time to system and Witty Pi...'
-  #force_ntp_update
-  #system_to_rtc
-#else
-  # get system year
-  sysyear="$(date +%Y)"
-  if [[ $rtctime == *"1999"* ]] || [[ $rtctime == *"2000"* ]]; then
-    # if you never set RTC time before
-    log 'RTC time has not been set before (stays in year 1999/2000).'
-    if [[ $sysyear != *"1969"* ]] && [[ $sysyear != *"1970"* ]]; then
-      # your Raspberry Pi has a decent time
-      system_to_rtc
-    else
-      log 'Neither system nor Witty Pi contains correct time.'
-    fi
-  fi
-#fi
-
 get_rtc_time()
 {
   local rtc_ts=$(get_rtc_timestamp)
@@ -61,3 +33,31 @@ get_rtc_timestamp()
     echo $rtctimestamp
   fi
 }
+
+# get RTC time
+rtctime="$(get_rtc_time)"
+
+# if RTC time is OK, write RTC time to system first
+if [[ $rtctime != *"1999"* ]] && [[ $rtctime != *"2000"* ]]; then
+  rtc_to_system
+fi
+
+#if $(has_internet) ; then
+ # # now take new time from NTP
+  #log 'Internet detected, apply NTP time to system and Witty Pi...'
+  #force_ntp_update
+  #system_to_rtc
+#else
+  # get system year
+  sysyear="$(date +%Y)"
+  if [[ $rtctime == *"1999"* ]] || [[ $rtctime == *"2000"* ]]; then
+    # if you never set RTC time before
+    log 'RTC time has not been set before (stays in year 1999/2000).'
+    if [[ $sysyear != *"1969"* ]] && [[ $sysyear != *"1970"* ]]; then
+      # your Raspberry Pi has a decent time
+      system_to_rtc
+    else
+      log 'Neither system nor Witty Pi contains correct time.'
+    fi
+  fi
+#fi

f11a48606ed480996dd711e20f7af5cd2e09237f
slomusti <musti@wlan-si.net>
2017-11-07 15:01:00 +0100

    dev

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Enable i2c - needed for the Display-O-Tron HAT
-modprobe i2c-dev && python src/main.py &
+modprobe i2c-dev
 
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 ./wifi-connect --clear=false
 
-echo "test"
-
 # At this point the WiFi connection has been configured and the device has
 # internet - unless the configured WiFi connection is no longer available.
+
+# Start the main application
+python src/main.py
+
+wait

5fc09b49de5e9fd2d8f8357b337282243ced78cc
slomusti <musti@wlan-si.net>
2017-11-07 15:04:33 +0100

    dev

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ modprobe i2c-dev
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 ./wifi-connect --clear=false
 
+echo "wifi script started, running gpio"
+
 # At this point the WiFi connection has been configured and the device has
 # internet - unless the configured WiFi connection is no longer available.
 

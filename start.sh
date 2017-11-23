a0896e67cb137a3e5fb6596f99dad6242ccc8ef1
Jernej Kos <jernej@kos.mx>
2017-11-23 11:44:07 +0100

    Power off HDMI

--- start.sh
+++ start.sh
@@ -9,6 +9,9 @@ modprobe bcm2835-v4l2
 # Setup host DBUS socket location, which is needed for NetworkManager.
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+# Power off HDMI.
+tvservice -o
+
 # Sync clock from RTC module.
 ./scripts/sync_time.sh
 

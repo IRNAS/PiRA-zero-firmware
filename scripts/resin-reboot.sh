1b52e261aa0d800a1bdd54bd3718ddaae8a60791
slomusti <musti@wlan-si.net>
2018-02-08 11:44:08 +0100

    added curl restart and shutdown commands

--- scripts/resin-reboot.sh
+++ scripts/resin-reboot.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-url -X POST --header "Content-Type:application/json" \
-    "$RESIN_SUPERVISOR_ADDRESS/v1/reboot?apikey=$RESIN_SUPERVISOR_API_KEY"
+curl -X POST --header "Content-Type:application/json" "$RESIN_SUPERVISOR_ADDRESS/v1/reboot?apikey=$RESIN_SUPERVISOR_API_KEY"
--- scripts/resin-shutdown.sh
+++ scripts/resin-shutdown.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-url -X POST --header "Content-Type:application/json" \
+curl -X POST --header "Content-Type:application/json" \
     "$RESIN_SUPERVISOR_ADDRESS/v1/shutdown?apikey=$RESIN_SUPERVISOR_API_KEY"

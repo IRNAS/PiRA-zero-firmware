784738ccee588536214e239b24432f8f721574fd
slomusti <musti@wlan-si.net>
2018-02-12 17:15:06 +0100

    updating reboot scripts for retying

--- scripts/resin-reboot.sh
+++ scripts/resin-reboot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST --header "Content-Type:application/json" "$RESIN_SUPERVISOR_ADDRESS/v1/reboot?apikey=$RESIN_SUPERVISOR_API_KEY"
+for i in 1 2 3 4 5; do curl -X POST --header "Content-Type:application/json" "$RESIN_SUPERVISOR_ADDRESS/v1/reboot?apikey=$RESIN_SUPERVISOR_API_KEY" && break || sleep 15; done
